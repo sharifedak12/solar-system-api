6048115de142fecec574b2e9f94253f89420bcdc
Joanna Dudley <jfdudleybmc@gmail.com>
2022-04-22 13:57:15 -0400

    Created Planet class and list of planets.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
 
+class Planets:
+    def __init__(self, id, name, description, has_moons):
+        self.id = id
+        self.name = name
+        self.desciption = description
+        self.has_moons = has_moons
+    
+planets = [
+    Planets(1, "Arrakis", "A desert planet, source of Spice.", True),
+    Planets(2, "Bela Tegeuse", "The fifth planet of Keuntsing.", False),
+    Planets(3, "Tupile", "Sanctuary planet for defeated houses of the Imperium.", True),
+    Planets(4, "Ix", "Supreme machine culture.", False)
+]
