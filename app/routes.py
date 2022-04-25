2eed63818b8db0392ee4a68375971599e12df144
Shari Fedak <90287794+sharifedak12@users.noreply.github.com>
2022-04-25 15:03:17 -0400

    passed wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
-from crypt import methods
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, has_moons):
@@ -8,6 +7,13 @@ class Planet:
         self.desciption = description
         self.has_moons = has_moons
     
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.desciption,
+            has_moons=self.has_moons)
+
 planets = [
     Planet(1, "Arrakis", "A desert planet, source of Spice.", True),
     Planet(2, "Bela Tegeuse", "The fifth planet of Keuntsing.", False),
@@ -20,11 +26,24 @@ planets_bp = Blueprint("Planets", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=("GET",))
 def get_all_planets():
     all_planets = [
-        dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.desciption,
-            has_moons = planet.has_moons
-        )for planet in planets
+        planet.to_dict()
+        for planet in planets
     ]
     return jsonify(all_planets)
+
+@planets_bp.route("/<id>", methods=("GET",))
+def handle_planet(id):
+    planet = validate_planet(id)
+    return jsonify(planet.to_dict())
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        abort(make_response(jsonify(dict(details=f"Planet {planet_id} is invalid!")),400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    abort(make_response(jsonify(dict(details=f"Planet {planet_id} not found.")),404))
