1c2964ed3a2ff40a4ec734b8139bf5fcb4018ab6
Shari Fedak <90287794+sharifedak12@users.noreply.github.com>
2022-04-29 15:53:38 -0400

    fixed typo in planet model, created new migration

--- app/models/planet.py
+++ app/models/planet.py
@@ -3,12 +3,12 @@ from app import db
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    desciption = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     has_moons = db.Column(db.Boolean, nullable=False)
     
     def to_dict(self):
         return dict(
             id=self.id,
             name=self.name,
-            description=self.desciption,
+            description=self.description,
             has_moons=self.has_moons)
