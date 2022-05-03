85106acf6c3c5c0e2b7d5b41502eccf3b1d6133c
Shari Fedak <90287794+sharifedak12@users.noreply.github.com>
2022-05-03 14:02:53 -0400

    passed wave 4

--- app/routes.py
+++ app/routes.py
@@ -33,21 +33,58 @@ def discover_planet():
 
     return make_response(f"Planet {new_planet.name} successfully added to the Planets Database.", 201)
 
+def get_planet_record_by_id(id):
+    try:
+        id = int(id)
+    except ValueError:
+        abort(make_response(jsonify(dict(details=f"Invalid id: {id}")),400))
+    planet = Planet.query.get(id)
+    if planet:
+        return planet
+    else:
+        abort(make_response(jsonify(dict(details=f"Planet id: {id} not found")),404))
 
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = get_planet_record_by_id(planet_id)
+    return jsonify(planet.to_dict())
 
-# @planets_bp.route("/<id>", methods=("GET",))
-# def handle_planet(id):
-#     planet = validate_planet(id)
-#     return jsonify(planet.to_dict())
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def replace_planet_by_id(planet_id):
+    planet = get_planet_record_by_id(planet_id)
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except ValueError:
-#         abort(make_response(jsonify(dict(details=f"Planet {planet_id} is invalid!")),400))
+    request_body = request.get_json()
 
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.has_mooons = request_body["has_moons"]
+
+    db.session.commit()
+
+    return make_response(jsonify(planet.to_dict()))
+
+@planets_bp.route("/<planet_id>", methods=["PATCH"])
+def update_planet_by_id(planet_id):
+    planet = get_planet_record_by_id(planet_id)
+
+    request_body = request.get_json()
+    planet_keys = request_body.keys()
     
-#     abort(make_response(jsonify(dict(details=f"Planet {planet_id} not found.")),404))
+    if "name" in planet_keys:
+        planet.name = request_body["name"]
+    if "description" in planet_keys:
+        planet.description = request_body["description"]
+    if "has_moons" in planet_keys:
+        planet.has_moons = request_body["has_moons"]
+
+    db.session.commit() 
+    return make_response(jsonify(planet.to_dict()))
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = get_planet_record_by_id(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(jsonify(dict(details=f"Planet {planet.name} successfully deleted from the Planets Database.")))
